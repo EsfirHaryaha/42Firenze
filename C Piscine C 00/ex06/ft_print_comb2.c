782889ddcbab13dd53e1e844d58ba60b0547af8c
Esfir Haryaha <eharyaha@c1r1p3.42firenze.it>
2025-10-22 12:46:45 +0200

    ex06

--- C Piscine C 00/ex06/ft_print_comb2.c
+++ C Piscine C 00/ex06/ft_print_comb2.c
@@ -12,58 +12,61 @@
 
 #include <unistd.h>
 
-void	ft_print_second_comb(char a, char b, char c, char d);
+void	ft_print_caratteri(char a, char b, char c, char d);
+void	calcolo_secondo_numero(char a, char b);
 
 void	ft_print_comb2(void)
 {
 	char	a;
 	char	b;
+
+	a = '0';
+	while (a <= '9')
+	{
+		b = '0';
+		while (b <= '9')
+		{
+			calcolo_secondo_numero(a, b);
+			b++;
+		}
+		a++;
+	}
+}
+
+void	calcolo_secondo_numero(char a, char b)
+{
 	char	c;
 	char	d;
 
-	a = '0';
-	b = '0';
-	c = '0';
-	while (a <= '9')
+	c = a;
+	while (c <= '9')
 	{
-		while (b <= '8')
+		if (c == a)
 		{
-			while (c <= '9')
-			{
-				while (d <= '9')
-				{
-					d = b + 1;
-					ft_print_caratteri( a, b, c, d);
-					d++;
-				}
-				d = c + 1;
-				c++;
-			}
-			c = b + 1;
-			b++;
+			d = b + 1;
 		}
-		b = a + 1;
-		a++;
+		else
+		{
+			d = '0';
+		}
+		while (d <= '9')
+		{
+			ft_print_caratteri(a, b, c, d);
+			d++;
+		}
+		c++;
 	}
 }
 
 void	ft_print_caratteri(char a, char b, char c, char d)
 {
-	if (a != c || b != d)
-	{
-		write(1, &a, 1);
-		write(1, &b, 1);
-		write(1, " ", 1);
-		write(1, &c, 1);
-		write(1, &d, 1);
-		if (a == '9' && b == '8' && c == '9' && d == '9')
-		{
-		}
-		else
-		{
-			write(1, ", ", 2);
-		}
-	}
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, " ", 1);
+	write(1, &c, 1);
+	write(1, &d, 1);
+	if (!(a == '9' && b == '8' && c == '9' && d == '9'))
+		write(1, ", ", 2);
 }
 
 int	main(void)
