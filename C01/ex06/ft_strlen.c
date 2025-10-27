2be4074ffacd4a0135afef991c426b0ee5662ccf
Esfir Haryaha <eharyaha@c1r2p1.42firenze.it>
2025-10-27 15:04:35 +0100

    ds

---
+++ C01/ex06/ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eharyaha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 14:53:54 by eharyaha          #+#    #+#             */
+/*   Updated: 2025/10/27 14:53:59 by eharyaha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int	ft_strlen(char *str);
+
+int	main(int argc, char **argv)
+{
+	printf("caratteri: %d\n parametri %d\n", ft_strlen(argv[1]), argc);
+	return (0);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
