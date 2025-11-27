087869f427459bbc6ed3962532ae2ae0e056cf5c
Esfir Haryaha <eharyaha@c1r5p2.42firenze.it>
2025-11-27 14:01:50 +0100

    c

--- libft/main.c
+++ libft/main.c
@@ -1,11 +1,36 @@
 #include "libft.h"
 #include <unistd.h>
+#include <string.h>
+#include <stdio.h>
 
 int	ft_isalpha(int c);
+int	ft_isdigit(int c);
 
-int	main()
+int	main(int argc, char **argv)
 {
-	int n = ft_isalpha('Z');
-	write(1, &n, 1);
+	if(argc < 2)
+	{
+		printf("ERROR: passare il nome della funzione che si vuole testare\n");
+	}
+
+	if(strcmp(argv[1], "ft_isalpha") == 0)
+	{
+		char ris = ft_isalpha(argv[2][0]) + '0';
+		printf("%c\n", ris);
+		return (0);
+	}
+	
+	if(strcmp(argv[1], "ft_isdigit") == 0)
+	{
+		printf("input: 1 --> %d\n", ft_isdigit(1));
+		printf("input: 'a' --> %d\n", ft_isdigit('a'));
+		printf("input: '1' --> %d\n", ft_isdigit('1'));
+		printf("input: 0 --> %d\n", ft_isdigit(0));
+		return (0);
+	}
+	
+	
+	printf("Funzione '%s' non trovata\n", argv[1]);
 	return (0);
 }
+
